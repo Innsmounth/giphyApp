3f7de772387093640629974dd471783694de00a7
Innsmounth <jfretana@outlook.com>
2020-12-08 02:04:07 -0600

    Actualizado

--- src/helpers/getGif.js
+++ src/helpers/getGif.js
@@ -1,6 +1,6 @@
 export const getGif = async(valor) =>{
         
-        const url = `http://api.giphy.com/v1/gifs/search?api_key=xTEP09ggxEaxyFAAnogp9Pt441VOS2XS&q=${encodeURI(valor)}&limit=5`
+        const url = `https://api.giphy.com/v1/gifs/search?api_key=xTEP09ggxEaxyFAAnogp9Pt441VOS2XS&q=${encodeURI(valor)}&limit=5`
         const response =  await fetch(url)
         const {data} = await response.json();
         // console.log(data.data)
